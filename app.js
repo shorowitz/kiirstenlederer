11b411a8c21d2a880a29d01d764b36880d321832
Sara Horowitz <sslhorowitz@gmail.com>
2017-01-19 16:00:49 -0500

    turned off auto-scrolling

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
   console.log('script is linked')
+  // if ( $(window).width() < 800 || window.Touch) {
+  //                     var $sensitivity = 10;
+  //                 } else{
+  //                     var $sensitivity = 100;
+  //                 };
+
     $('#fullpage').fullpage({
         anchors:['home', 'bio', 'skills', 'testimonials', 'contact'],
         slidesNavigation: true,
         menu: '#myMenu',
         scrollBar: true,
+        autoScrolling: false
     });
   });
-  
